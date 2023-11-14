c5efdc234cec2e30bc29cff60c992d495d1fbc94
Meriellen Silveira <merysilveira@gmail.com>
2023-11-14 18:26:08 -0300

    conclusão da configuração do JWT

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -25,7 +25,6 @@ const endpointLogin = async (
       const usuarioEncontrado = usuariosEncontrados[0];
 
       const token = jwt.sign({ id: usuarioEncontrado._id }, MINHA_CHAVE_JWT);
-
       return res.status(200).json({ 
         nome: usuarioEncontrado.nome, 
         email: usuarioEncontrado.email, 
